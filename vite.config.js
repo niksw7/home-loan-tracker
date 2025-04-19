bde96fa2c9d39c0e98b4396481f4231e52d1d6ee
nikesh <nikesh@nikeshs-MacBook-Pro-2.local>
2025-04-19 19:43:56 +0530

    add dist

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/home-loan-tracker/',
+  base: '/niksw7/',
   plugins: [react()],
 });
