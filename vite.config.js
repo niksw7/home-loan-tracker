226bebf5292e5a3c7ffe3470d7104d46522ba676
nikesh <nikesh@nikeshs-MacBook-Pro-2.local>
2025-04-19 23:53:45 +0530

    add dist

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+// vite.config.js
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-export default ({
-  base: '/home-loan-tracker/',
+export default defineConfig({
+  base: '/home-loan-tracker/', // 👈 your GitHub repo name
   plugins: [react()],
-});
+})
